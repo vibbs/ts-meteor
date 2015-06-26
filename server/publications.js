e13a625eea2278a6cadd9d945ea4c550c4574e0f
vibbs <vibbs.dod@gmail.com>
2015-06-26 15:21:44 -0400

    debig publish username try 1

--- server/publications.js
+++ server/publications.js
@@ -17,5 +17,7 @@ Meteor.publish('categories', function(){
 	return Categories.find();
 });
 Meteor.publish('users', function(){
-	return Meteor.users.find();
+	return Meteor.users.find({},{
+		fields : {username :1}
+	});
 });
